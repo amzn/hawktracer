6a2ea52864870ebbfe28cd912d09f616a348bef2
Marcin Kolny <mkolny@amazon.com>
2017-07-07 16:38:41 +0100

    tools: set category of labels from binary to 2 (which is native)

--- tools/generate_map_from_binary.sh
+++ tools/generate_map_from_binary.sh
@@ -3,14 +3,16 @@
 readelf_path=readelf
 min_address=1
 
+echoerr() { echo "$@" 1>&2; }
+
 function print_help {
-    echo "Usage: ./generate_map_from_binary.sh [OPTION]..."
-    echo "Generate hawktracer map output with function addresses from binary file"
-    echo ""
-    echo "  --binary=BINARY_FILE        path to a binary file"
-    echo "  --readelf=READELF_BIN       path to a readelf application"
-    echo "  --min-address=VALUE         minimum functinon address that will be"
-    echo "                              considered in the output"
+    echoerr "Usage: ./generate_map_from_binary.sh [OPTION]..."
+    echoerr "Generate hawktracer map output with function addresses from binary file"
+    echoerr ""
+    echoerr "  --binary=BINARY_FILE        path to a binary file"
+    echoerr "  --readelf=READELF_BIN       path to a readelf application"
+    echoerr "  --min-address=VALUE         minimum functinon address that will be"
+    echoerr "                              considered in the output"
 }
 
 for i in "$@"
@@ -33,4 +35,16 @@ do
     shift
 done
 
-$readelf_path -s --wide $binary_path | grep " FUNC " | gawk '{print strtonum("0x" $2) " " $8}' | awk '{system("c++filt" $3)}' | grep '^[0-9]' | awk -v min_address=$min_address '{ if ($1 >= min_address) print $1 " 0 " $2 }' 
+if [ -z "$binary_path" ]
+then
+    echoerr "Option --binary is required!"
+    exit 1
+fi
+
+echoerr "Generating address map for $binary_path"
+echoerr "Minimum address: $min_address"
+echoerr "Readelf application: $readelf_path"
+
+$readelf_path -s --wide $binary_path | grep " FUNC " | gawk '{print strtonum("0x" $2) " " $8}' | awk '{system("c++filt" $3)}' | grep '^[0-9]' | awk -v min_address=$min_address '{ if ($1 >= min_address) print $1 " 2 " $2 }' 
+
+echoerr "Generating map completed!"
