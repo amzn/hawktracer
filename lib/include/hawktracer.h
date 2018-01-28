589cd003870f79e37336b3a6ffb4e690f178b981
Marcin Kolny <marcin.kolny@gmail.com>
2018-01-28 19:00:16 +0000

    [lib] add main header files

---
+++ lib/include/hawktracer.h
@@ -0,0 +1,29 @@
+#ifndef HAWKTRACER_HAWKTRACER_H
+#define HAWKTRACER_HAWKTRACER_H
+
+#include <hawktracer/alloc.h>
+#include <hawktracer/bag.h>
+#include <hawktracer/base_types.h>
+#include <hawktracer/callstack_scoped_tracepoint.h>
+#include <hawktracer/callstack_scoped_tracepoint.hpp>
+#include <hawktracer/core_events.h>
+#include <hawktracer/event_id_provider.h>
+#include <hawktracer/event_macros.h>
+#include <hawktracer/events.h>
+#include <hawktracer/feature_cached_string.h>
+#include <hawktracer/feature_callstack.h>
+#include <hawktracer/global_timeline.h>
+#include <hawktracer/hash.h>
+#include <hawktracer/init.h>
+#include <hawktracer/listeners.h>
+#include <hawktracer/macros.h>
+#include <hawktracer/mkcreflect.h>
+#include <hawktracer/monotonic_clock.h>
+#include <hawktracer/registry.h>
+#include <hawktracer/simple_init.h>
+#include <hawktracer/stack.h>
+#include <hawktracer/thread.h>
+#include <hawktracer/timeline.h>
+#include <hawktracer/timeline_listener.h>
+
+#endif /* HAWKTRACER_HAWKTRACER_H */
---
+++ lib/include/hawktracer/listeners.h
@@ -0,0 +1,6 @@
+#ifndef HAWKTRACER_LISTENERS_H
+#define HAWKTRACER_LISTENERS_H
+
+#include <hawktracer/listeners/file_dump_listener.h>
+
+#endif /* HAWKTRACER_LISTENERS_H */
