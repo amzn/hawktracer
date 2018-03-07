cb6042e434ed5f7d7ad13ec7a551b00b16e88cba
Marcin Kolny <mkolny@amazon.com>
2018-03-07 17:50:32 +0000

    [lib] port tcp server to windows

--- lib/tcp_server.cpp
+++ lib/tcp_server.cpp
@@ -1,7 +1,12 @@
 #include "internal/listeners/tcp_server.hpp"
 
+#ifdef _WIN32
+#include <WinSock2.h>
+#pragma comment(lib, "Ws2_32.lib")
+#else
 #include <arpa/inet.h>
 #include <unistd.h>
+#endif
 
 #include <cstring>
 #include <vector>
@@ -28,7 +33,12 @@ void TCPServer::stop()
 
     int prev_sock_fd = _sock_fd;
     _sock_fd = -1;
+#ifdef _WIN32
+    closesocket(prev_sock_fd);
+    WSACleanup();
+#else
     shutdown(prev_sock_fd, 2);
+#endif
 
     if (_accept_client_thread.joinable())
     {
@@ -42,7 +52,13 @@ bool TCPServer::start(int port, OnClientConnected client_connected, void* user_d
     {
         stop();
     }
-
+#ifdef _WIN32
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        return false;
+    }
+#endif
     _sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (_sock_fd < 0)
@@ -50,14 +66,16 @@ bool TCPServer::start(int port, OnClientConnected client_connected, void* user_d
         return false;
     }
 
+#ifndef _WIN32
     int optval = 1;
-    sockaddr_in serveraddr;
     if (setsockopt(_sock_fd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int)) != 0)
     {
         stop();
         return false;
     }
+#endif
 
+    sockaddr_in serveraddr;
     memset((char *) &serveraddr, 0, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -111,7 +129,7 @@ bool TCPServer::write_to_socket(int sock_fd, char* buffer, size_t size)
 
     while (sent < size)
     {
-        int n = ::write(sock_fd, buffer + sent, size - sent);
+        int n = send(sock_fd, buffer + sent, size - sent, 0);
 
         if (n <= 0)
         {
@@ -129,7 +147,12 @@ void TCPServer::_run(OnClientConnected client_connected, void* user_data)
     while (is_running())
     {
         struct sockaddr_in clientaddr;
-        socklen_t client_len = sizeof(clientaddr);
+#ifdef _WIN32
+        int client_len;
+#else
+        socklen_t client_len;
+#endif
+        client_len = sizeof(clientaddr);
         int client_fd = accept(_sock_fd, (struct sockaddr *) &clientaddr, &client_len);
 
         if (client_fd >= 0)
