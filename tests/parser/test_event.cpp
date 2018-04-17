fd5bf1cc433cbaec13327554f5a0c4f7f0e65849
Marcin Kolny <marcin.kolny@gmail.com>
2018-04-17 22:15:50 +0100

    [tests] add tests for Event parser klass

--- tests/parser/test_event.cpp
+++ tests/parser/test_event.cpp
@@ -56,9 +56,9 @@ TEST_F(TestEvent, MergeEventsShouldCopyAllValues)
     Event event1(_klass);
     Event event2(_klass);
     const char* test_string = "aaa";
-    FieldType value1{}; value1.f_INT8 = 15;
-    FieldType value2{}; value2.f_STRING = (char*)malloc(4); strncpy(value2.f_STRING, test_string, 4);
-    FieldType value3{}; value3.f_UINT64 = 15;
+    int8_t value1 = 12;
+    char* value2 = (char*)malloc(4); strncpy(value2, test_string, 4);
+    uint64_t value3 = 15;
 
     event1.set_value(_klass_field1.get(), value1);
     event2.set_value(_klass_field2.get(), value2);
@@ -69,7 +69,72 @@ TEST_F(TestEvent, MergeEventsShouldCopyAllValues)
 
     // Assert
     ASSERT_EQ(3u, event1.get_values().size());
-    ASSERT_EQ(value1.f_INT8, event1.get_value<int8_t>(_klass_field1->get_name()));
+    ASSERT_EQ(value1, event1.get_value<int8_t>(_klass_field1->get_name()));
     ASSERT_STREQ(test_string, event1.get_value<char*>(_klass_field2->get_name()));
-    ASSERT_EQ(value3.f_UINT64, event1.get_value<uint64_t>(_klass_field3->get_name()));
+    ASSERT_EQ(value3, event1.get_value<uint64_t>(_klass_field3->get_name()));
+}
+
+TEST_F(TestEvent, CopyEventShouldCopyAllValues)
+{
+    // Arrange
+    auto base_klass = std::make_shared<EventKlass>("base_klass", 1);
+    auto base_klass_field1 = std::make_shared<EventKlassField>("base_field1", "field_type", FieldTypeId::INT8);
+    auto base_klass_field2 = std::make_shared<EventKlassField>("base_field2", "field_type", FieldTypeId::STRING);
+    auto base_klass_field3 = std::make_shared<EventKlassField>("base_field3", "klass_name", FieldTypeId::STRUCT);
+
+    Event base_event(base_klass);
+    const char* test_string = "abc";
+    int8_t base_value1 = 12;
+    char* base_value2 = (char*)malloc(4); strncpy(base_value2, test_string, 4);
+
+    const char* test_string2 = "xyz";
+    int8_t value1 = 12;
+    char* value2 = (char*)malloc(4); strncpy(value2, test_string2, 4);
+    uint64_t value3 = 15;
+    auto base_value3 = new Event(_klass);
+    base_value3->set_value(_klass_field1.get(), value1);
+    base_value3->set_value(_klass_field2.get(), value2);
+    base_value3->set_value(_klass_field3.get(), value3);
+
+    base_event.set_value(base_klass_field1.get(), base_value1);
+    base_event.set_value(base_klass_field2.get(), base_value2);
+    base_event.set_value(base_klass_field3.get(), base_value3);
+
+    // Act
+    Event copy_event = base_event;
+
+    // Assert
+    ASSERT_EQ(base_value1, copy_event.get_value<int8_t>(base_klass_field1->get_name()));
+    ASSERT_STREQ(test_string, copy_event.get_value<char*>(base_klass_field2->get_name()));
+    Event* parent = copy_event.get_value<Event*>(base_klass_field3->get_name());
+    ASSERT_EQ(value1, parent->get_value<int8_t>(_klass_field1->get_name()));
+    ASSERT_STREQ(test_string2, parent->get_value<char*>(_klass_field2->get_name()));
+    ASSERT_EQ(value3, parent->get_value<uint64_t>(_klass_field3->get_name()));
+}
+
+TEST_F(TestEvent, CopyEventWithNullStringShouldSetFieldValueToNull)
+{
+    // Arrange
+    Event event(_klass);
+    event.set_value<char*>(_klass_field2.get(), nullptr);
+
+    // Act
+    Event event_copy = event;
+
+    // Assert
+    ASSERT_EQ(nullptr, event_copy.get_value<char*>(_klass_field2->get_name()));
+}
+
+TEST_F(TestEvent, CopyEventWithNullEventShouldSetFieldValueToNull)
+{
+    // Arrange
+    Event event(_klass);
+    auto field = std::make_shared<EventKlassField>("base_field3", "klass_name", FieldTypeId::STRUCT);
+    event.set_value<Event*>(field.get(), nullptr);
+
+    // Act
+    Event event_copy = event;
+
+    // Assert
+    ASSERT_EQ(nullptr, event_copy.get_value<Event*>(field->get_name()));
 }
