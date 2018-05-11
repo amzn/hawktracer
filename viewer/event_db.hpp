223db85d6edef5a1ed057f750455517af43a5f89
Maria Pandele <mpandel@amazon.com>
2018-05-11 14:07:50 +0100

    [viewer] splitted get_data into multiple methods

--- viewer/event_db.hpp
+++ viewer/event_db.hpp
@@ -26,19 +26,34 @@ public:
 
     void insert(parser::Event event);
 
-    std::vector<EventRef> get_data(HT_TimestampNs start_ts, HT_TimestampNs stop_ts, const Query& query);
+    std::vector<EventRef> get_data(HT_TimestampNs start_ts,
+                                   HT_TimestampNs stop_ts,
+                                   const Query& query);
 
 private:
     std::unordered_map<HT_EventKlassId, std::vector<parser::Event>> _events;
 
-    class DummyEvent : public parser::Event
+    class CompareTimestampEvent : public parser::Event
     {
     public:
-        DummyEvent(HT_TimestampNs timestamp)
+        CompareTimestampEvent(HT_TimestampNs timestamp)
         {
             _timestamp = timestamp;
         }
     };
+
+    void query_all_event_klasses(std::vector<EventRef>& response,
+                                 HT_TimestampNs start_ts,
+                                 HT_TimestampNs stop_ts);
+    void query_event_klass(std::vector<EventRef>& response,
+                           std::vector<parser::Event>& events,
+                           HT_TimestampNs start_ts,
+                           HT_TimestampNs stop_ts);
+    void get_range_of_events(std::vector<parser::Event>& events,
+                             HT_TimestampNs start_ts,
+                             HT_TimestampNs stop_ts,
+                             std::vector<parser::Event>::iterator& first_event,
+                             std::vector<parser::Event>::iterator& last_event);
 };
 
 } // namespace viewer
