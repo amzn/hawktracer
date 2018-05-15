cd4137fc75b79e62293aa4e8a045b462c0f9708d
Marcin Kolny <marcin.kolny@gmail.com>
2018-05-16 00:31:08 +0100

    [lib] replace init/deinit with create/destroy in tcp listener

--- lib/include/hawktracer/listeners/tcp_listener.h
+++ lib/include/hawktracer/listeners/tcp_listener.h
@@ -7,20 +7,13 @@
 
 HT_DECLS_BEGIN
 
-struct _HT_TCPListenerPimpl;
-
-typedef struct _HT_TCPListenerPimpl HT_TCPListenerPimpl;
-
 #define HT_TCP_LISTENER_BUFFER_SIZE 4096 /* TODO make configurable */
 
-typedef struct
-{
-    HT_TCPListenerPimpl *pimpl;
-} HT_TCPListener;
+typedef struct _HT_TCPListener HT_TCPListener;
 
-HT_API HT_ErrorCode ht_tcp_listener_init(HT_TCPListener* listener, int port);
+HT_API HT_TCPListener* ht_tcp_listener_create(int port, HT_ErrorCode* out_err);
 
-HT_API void ht_tcp_listener_deinit(HT_TCPListener* listener);
+HT_API void ht_tcp_listener_destroy(HT_TCPListener* listener);
 
 HT_API void ht_tcp_listener_callback(TEventPtr events, size_t size, HT_Boolean serialized, void* user_data);
 
