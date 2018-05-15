006111f99e71581cc3599e92d55541ae4c07fe69
Maria Pandele <mpandel@amazon.com>
2018-05-15 15:34:34 +0100

    Revert "[viewer] Cache::get_data returns boost::optional"
    This reverts commit 95077e595f3e7f491fa6c065efc7f57d7c7b2d55.

--- viewer/cache.hpp
+++ viewer/cache.hpp
@@ -4,8 +4,6 @@
 #include <hawktracer/base_types.h>
 #include <hawktracer/parser/event.hpp>
 
-#include <boost/optional.hpp>
-
 namespace HawkTracer
 {
 namespace viewer
@@ -20,7 +18,7 @@ public:
                         HT_TimestampNs stop_ts,
                         HT_EventKlassId klass_id);
 
-    boost::optional<std::vector<EventRef>> get_data(HT_EventKlassId klass_id);
+    std::vector<EventRef> get_data(HT_EventKlassId klass_id);
 
     void update(HT_EventKlassId klass_id,
                 std::vector<EventRef> new_response);
