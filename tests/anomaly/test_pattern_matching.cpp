708b6c57bb1ddbd51d7a8422782a808319881e2f
Maria Pandele <mpandel@amazon.com>
2018-06-18 15:32:04 +0100

    [tests] moved the tests implementations into a different method

--- tests/anomaly/test_pattern_matching.cpp
+++ tests/anomaly/test_pattern_matching.cpp
@@ -2,120 +2,70 @@
 #include <tests/client/test_path.hpp>
 #include <gtest/gtest.h>
 
-TEST(TestPatternMatching, TestOneRelabel)
+void test_pattern_matching(std::string pattern_file_name,
+                           std::string config_file_name,
+                           std::string source_file_name,
+                           std::vector<std::vector<int>> results)
 {
     auto patterns = std::make_shared<HawkTracer::anomaly::Graphs>();
-    patterns->load_from_file(TestPath::get().get_input_file_path("test1_pattern.json"));
+    patterns->load_from_file(TestPath::get().get_input_file_path(pattern_file_name));
 
     auto config = std::make_shared<HawkTracer::anomaly::Config>();
-    config->load_from_file(TestPath::get().get_input_file_path("test1_config.json"));
+    config->load_from_file(TestPath::get().get_input_file_path(config_file_name));
 
     HawkTracer::anomaly::PatternMatching pattern_matcher(config, patterns);
     auto source = std::make_shared<HawkTracer::anomaly::Graphs>();
-    source->load_from_file(TestPath::get().get_input_file_path("test1_source.json"));
+    source->load_from_file(TestPath::get().get_input_file_path(source_file_name));
     
     const auto& sources = source->get_trees();
-    ASSERT_TRUE(patterns->get_trees().size() == 1);
-    ASSERT_TRUE(sources.size() == 1);
-    for (const auto& source : sources)
+    for (size_t i = 0; i < sources.size(); ++i)
     {
-        auto ans = pattern_matcher.get_matching_scores(source.first);
-        ASSERT_TRUE(ans.size() == 1);
-        ASSERT_TRUE(ans[0] == 2);
+        auto ans = pattern_matcher.get_matching_scores(sources[i].first);
+        ASSERT_EQ(results[i].size(), ans.size());
+        for (size_t j = 0; j < ans.size(); ++j)
+        {
+            ASSERT_EQ(results[i][j], ans[j]);
+        }
     }
+}
+
+TEST(TestPatternMatching, TestOneRelabel)
+{
+    test_pattern_matching("test1_pattern.json",
+                          "test1_config.json",
+                          "test1_source.json",
+                          {{2}});
 }
 
 TEST(TestPatternMatching, TestSameTree)
 {
-    auto patterns = std::make_shared<HawkTracer::anomaly::Graphs>();
-    patterns->load_from_file(TestPath::get().get_input_file_path("test2_pattern.json"));
-
-    auto config = std::make_shared<HawkTracer::anomaly::Config>();
-    config->load_from_file(TestPath::get().get_input_file_path("test2_config.json"));
-
-    HawkTracer::anomaly::PatternMatching pattern_matcher(config, patterns);
-    auto source = std::make_shared<HawkTracer::anomaly::Graphs>();
-    source->load_from_file(TestPath::get().get_input_file_path("test2_source.json"));
-    
-    const auto& sources = source->get_trees();
-    ASSERT_TRUE(patterns->get_trees().size() == 1);
-    ASSERT_TRUE(sources.size() == 1);
-    for (const auto& source : sources)
-    {
-        auto ans = pattern_matcher.get_matching_scores(source.first);
-        ASSERT_TRUE(ans.size() == 1);
-        ASSERT_TRUE(ans[0] == 0);
-    }
+    test_pattern_matching("test2_pattern.json",
+                          "test2_config.json",
+                          "test2_source.json",
+                          {{0}});
 }
 
 TEST(TestPatternMatching, TestOneDelete)
 {
-    auto patterns = std::make_shared<HawkTracer::anomaly::Graphs>();
-    patterns->load_from_file(TestPath::get().get_input_file_path("test3_pattern.json"));
-
-    auto config = std::make_shared<HawkTracer::anomaly::Config>();
-    config->load_from_file(TestPath::get().get_input_file_path("test3_config.json"));
-
-    HawkTracer::anomaly::PatternMatching pattern_matcher(config, patterns);
-    auto source = std::make_shared<HawkTracer::anomaly::Graphs>();
-    source->load_from_file(TestPath::get().get_input_file_path("test3_source.json"));
-    
-    const auto& sources = source->get_trees();
-    ASSERT_TRUE(patterns->get_trees().size() == 1);
-    ASSERT_TRUE(sources.size() == 1);
-    for (const auto& source : sources)
-    {
-        auto ans = pattern_matcher.get_matching_scores(source.first);
-        ASSERT_TRUE(ans.size() == 1);
-        ASSERT_TRUE(ans[0] == 20);
-    }
+    test_pattern_matching("test3_pattern.json",
+                          "test3_config.json",
+                          "test3_source.json",
+                          {{20}});
 }
 
 TEST(TestPatternMatching, TestOneDeleteAndOneInsert)
 {
-    auto patterns = std::make_shared<HawkTracer::anomaly::Graphs>();
-    patterns->load_from_file(TestPath::get().get_input_file_path("test4_pattern.json"));
-    ASSERT_TRUE(patterns->get_trees().size() == 1);
-
-    auto config = std::make_shared<HawkTracer::anomaly::Config>();
-    config->load_from_file(TestPath::get().get_input_file_path("test4_config.json"));
-
-    HawkTracer::anomaly::PatternMatching pattern_matcher(config, patterns);
-    auto source = std::make_shared<HawkTracer::anomaly::Graphs>();
-    source->load_from_file(TestPath::get().get_input_file_path("test4_source.json"));
-    
-    const auto& sources = source->get_trees();
-    ASSERT_TRUE(patterns->get_trees().size() == 1);
-    ASSERT_TRUE(sources.size() == 1);
-    for (const auto& source : sources)
-    {
-        auto ans = pattern_matcher.get_matching_scores(source.first);
-        ASSERT_TRUE(ans.size() == 1);
-        ASSERT_TRUE(ans[0] == 30);
-    }
+    test_pattern_matching("test4_pattern.json",
+                          "test4_config.json",
+                          "test4_source.json",
+                          {{30}});
 }
 
 TEST(TestPatternMatching, TestReverseChildrenOrder)
 {
-    auto patterns = std::make_shared<HawkTracer::anomaly::Graphs>();
-    patterns->load_from_file(TestPath::get().get_input_file_path("test5_pattern.json"));
-
-    auto config = std::make_shared<HawkTracer::anomaly::Config>();
-    config->load_from_file(TestPath::get().get_input_file_path("test5_config.json"));
-
-    HawkTracer::anomaly::PatternMatching pattern_matcher(config, patterns);
-    auto source = std::make_shared<HawkTracer::anomaly::Graphs>();
-    source->load_from_file(TestPath::get().get_input_file_path("test5_source.json"));
-    
-    const auto& sources = source->get_trees();
-    ASSERT_TRUE(patterns->get_trees().size() == 1);
-    ASSERT_TRUE(sources.size() == 1);
-    for (const auto& source : sources)
-    {
-        auto ans = pattern_matcher.get_matching_scores(source.first);
-        printf("%d\n", ans[0]);
-        ASSERT_TRUE(ans.size() == 1);
-        ASSERT_TRUE(ans[0] == 24);
-    }
+    test_pattern_matching("test5_pattern.json",
+                          "test5_config.json",
+                          "test5_source.json",
+                          {{24}});
 }
 
