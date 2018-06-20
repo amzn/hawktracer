edaffea2b5ab61406b421a594a8a92da83601b3b
Maria Pandele <mpandel@amazon.com>
2018-06-20 15:50:57 +0100

    [anomaly] removed tree file loader

--- tests/client/test_file_loader.hpp
+++ tests/client/test_file_loader.hpp
@@ -1,17 +1,29 @@
 #ifndef HT_TEST_CLIENT_TESTFILELOADER_HPP
 #define HT_TEST_CLIENT_TESTFILELOADER_HPP
 
-#include <anomaly/tree_file_loader.hpp>
+#include <client/call_graph.hpp>
+#include <fstream>
 
-class TestFileLoader : public HawkTracer::anomaly::TreeFileLoader
+using HawkTracer::client::CallGraph;
+
+class TestFileLoader 
 {
 
 public:
+    bool init(std::string file_name); 
     std::vector<CallGraph::NodeData> get_events();
+    std::vector<std::pair<std::shared_ptr<CallGraph::TreeNode>, int>> get_trees();
 
 private:
+    std::string _next_valid_line();
+    void _read_tree_nodes();
+    void _read_tree_edges();
     void _read_events_data();
-    void _parse_file() override;
+    void _parse_file();
+
+    std::ifstream _file;
+    std::unordered_map<unsigned int, std::pair<std::shared_ptr<HawkTracer::client::CallGraph::TreeNode>, unsigned int>> _nodes;
+    std::vector<std::pair<std::shared_ptr<CallGraph::TreeNode>, int>> _trees;
     std::vector<CallGraph::NodeData> _events;
 };
 
