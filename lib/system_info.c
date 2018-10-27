914b6c296724f85dd88bac75bcec83b1843b457b
Marcin Kolny <marcin.kolny@gmail.com>
2018-10-27 19:23:41 +0100

    [lib] fix uninitialized memory usage error

--- lib/system_info.c
+++ lib/system_info.c
@@ -13,11 +13,12 @@ ht_system_info_push_system_info_to_listener(HT_TimelineListenerCallback callback
 {
     size_t data_size;
     HT_DECL_EVENT(HT_SystemInfoEvent, event);
+    event.base.id = event.base.timestamp = 0;
     event.version_major = HT_VERSION_MAJOR;
     event.version_minor = HT_VERSION_MINOR;
     event.version_patch = HT_VERSION_PATCH;
 
-    HT_Byte buffer[1024];
+    HT_Byte buffer[64];
 
     data_size = ht_event_utils_serialize_event_to_buffer(HT_EVENT(&event), buffer, serialize);
 
@@ -31,6 +32,7 @@ ht_system_info_push_endianness_info_to_listener(HT_TimelineListenerCallback call
 {
     size_t data_size;
     HT_DECL_EVENT(HT_EndiannessInfoEvent, event);
+    event.base.id = event.base.timestamp = 0;
     event.endianness = (uint8_t) ht_system_info_get_endianness();
 
     HT_Byte buffer[64];
