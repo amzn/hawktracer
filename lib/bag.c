925e5df0265f91dc09753fcfa9b581fed953b4bd
Marcin Kolny <marcin.kolny@gmail.com>
2018-11-24 14:02:58 +0000

    [lib] define bag as a macro
    So we can easily use it for other types

--- lib/bag.c
+++ lib/bag.c
@@ -1,95 +1,98 @@
 #include "internal/bag.h"
 #include "hawktracer/alloc.h"
 
-static HT_INLINE HT_Boolean
-_ht_bag_resize(HT_Bag* bag, size_t new_capacity)
-{
-    void** ptr = (void**)ht_realloc(bag->data, new_capacity * sizeof(void*));
-
-    if (ptr == NULL)
-    {
-        return HT_FALSE;
+#define HT_DEFINE_BAG_TYPE(TYPE_SUFFIX, METHOD_SUFFIX, TYPE) \
+    static HT_INLINE HT_Boolean \
+    ht_bag##METHOD_SUFFIX##_resize_(HT_Bag##TYPE_SUFFIX* bag, size_t new_capacity) \
+    { \
+        TYPE* ptr = (TYPE*)ht_realloc(bag->data, new_capacity * sizeof(void*)); \
+        \
+        if (ptr == NULL) \
+        { \
+            return HT_FALSE; \
+        } \
+        \
+        bag->data = ptr; \
+        bag->capacity = new_capacity; \
+        \
+        return HT_TRUE; \
+    } \
+    \
+    HT_ErrorCode \
+    ht_bag##METHOD_SUFFIX##_init(HT_Bag##TYPE_SUFFIX* bag, size_t min_capacity) \
+    { \
+        bag->data = (TYPE*)ht_alloc(min_capacity * sizeof(TYPE)); \
+        \
+        if (bag->data == NULL) \
+        { \
+            return HT_ERR_OUT_OF_MEMORY; \
+        } \
+        \
+        bag->min_capacity = min_capacity; \
+        bag->capacity = min_capacity; \
+        bag->size = 0; \
+        \
+        return HT_ERR_OK; \
+    } \
+    \
+    void \
+    ht_bag##METHOD_SUFFIX##_remove(HT_Bag##TYPE_SUFFIX* bag, TYPE data) \
+    { \
+        size_t i; \
+        for (i = 0; i < bag->size; i++) \
+        { \
+            if (bag->data[i] == data) \
+            { \
+                ht_bag##METHOD_SUFFIX##_remove_nth(bag, i); \
+            } \
+        } \
+    } \
+    \
+    void \
+    ht_bag##METHOD_SUFFIX##_remove_nth(HT_Bag##TYPE_SUFFIX* bag, size_t n) \
+    { \
+        bag->size--; \
+        if (bag->size > 0) \
+        { \
+            bag->data[n] = bag->data[bag->size]; \
+        } \
+        \
+        if (bag->capacity > bag->min_capacity && bag->size < bag->capacity / 4) \
+        { \
+            ht_bag##METHOD_SUFFIX##_resize_(bag, bag->capacity / 2); \
+        } \
+    } \
+    \
+    HT_ErrorCode \
+    ht_bag##METHOD_SUFFIX##_add(HT_Bag##TYPE_SUFFIX* bag, TYPE data) \
+    { \
+        if (bag->capacity == bag->size) \
+        { \
+            if (ht_bag##METHOD_SUFFIX##_resize_(bag, bag->capacity * 2) == HT_FALSE) \
+            { \
+                return HT_ERR_OUT_OF_MEMORY; \
+            } \
+        } \
+        \
+        bag->data[bag->size++] = data; \
+        \
+        return HT_ERR_OK; \
+    } \
+    \
+    void \
+    ht_bag##METHOD_SUFFIX##_clear(HT_Bag##TYPE_SUFFIX* bag) \
+    { \
+        ht_bag##METHOD_SUFFIX##_resize_(bag, bag->min_capacity); \
+        bag->size = 0; \
+    } \
+    \
+    void \
+    ht_bag##METHOD_SUFFIX##_deinit(HT_Bag##TYPE_SUFFIX* bag) \
+    { \
+        ht_free(bag->data); \
+        bag->data = NULL; \
+        bag->capacity = 0; \
+        bag->size = 0; \
     }
 
-    bag->data = ptr;
-    bag->capacity = new_capacity;
-
-    return HT_TRUE;
-}
-
-HT_ErrorCode
-ht_bag_init(HT_Bag* bag, size_t min_capacity)
-{
-    bag->data = (void**)ht_alloc(min_capacity * sizeof(void*));
-
-    if (bag->data == NULL)
-    {
-        return HT_ERR_OUT_OF_MEMORY;
-    }
-
-    bag->min_capacity = min_capacity;
-    bag->capacity = min_capacity;
-    bag->size = 0;
-
-    return HT_ERR_OK;
-}
-
-void
-ht_bag_remove(HT_Bag* bag, void* data)
-{
-    size_t i;
-    for (i = 0; i < bag->size; i++)
-    {
-        if (bag->data[i] == data)
-        {
-            ht_bag_remove_nth(bag, i);
-        }
-    }
-}
-
-void
-ht_bag_remove_nth(HT_Bag* bag, size_t n)
-{
-    bag->size--;
-    if (bag->size > 0)
-    {
-        bag->data[n] = bag->data[bag->size];
-    }
-
-    if (bag->capacity > bag->min_capacity && bag->size < bag->capacity / 4)
-    {
-        _ht_bag_resize(bag, bag->capacity / 2);
-    }
-}
-
-HT_ErrorCode
-ht_bag_add(HT_Bag* bag, void* data)
-{
-    if (bag->capacity == bag->size)
-    {
-        if (_ht_bag_resize(bag, bag->capacity * 2) == HT_FALSE)
-        {
-            return HT_ERR_OUT_OF_MEMORY;
-        }
-    }
-
-    bag->data[bag->size++] = data;
-
-    return HT_ERR_OK;
-}
-
-void
-ht_bag_clear(HT_Bag* bag)
-{
-    _ht_bag_resize(bag, bag->min_capacity);
-    bag->size = 0;
-}
-
-void
-ht_bag_deinit(HT_Bag* bag)
-{
-    ht_free(bag->data);
-    bag->data = NULL;
-    bag->capacity = 0;
-    bag->size = 0;
-}
+HT_DEFINE_BAG_TYPE(,, void*)
--- lib/include/internal/bag.h
+++ lib/include/internal/bag.h
@@ -8,92 +8,26 @@
 
 HT_DECLS_BEGIN
 
-/**
- * A container holding pointers.
- */
-typedef struct
-{
-    /* private */
-    size_t min_capacity;
-    size_t capacity;
-    size_t size;
-    void** data;
-} HT_Bag;
+#define HT_DECLARE_BAG_TYPE(TYPE_SUFFIX, METHOD_SUFFIX, TYPE) \
+    typedef struct \
+    { \
+        /* private */ \
+        size_t min_capacity; \
+        size_t capacity; \
+        size_t size; \
+        TYPE* data; \
+    } HT_Bag##TYPE_SUFFIX; \
+    \
+    HT_API HT_ErrorCode ht_bag##METHOD_SUFFIX##_init(HT_Bag##TYPE_SUFFIX* bag, size_t min_capacity); \
+    HT_API void ht_bag##METHOD_SUFFIX##_deinit(HT_Bag##TYPE_SUFFIX* bag); \
+    HT_API void ht_bag##METHOD_SUFFIX##_remove(HT_Bag##TYPE_SUFFIX* bag, TYPE data); \
+    HT_API void ht_bag##METHOD_SUFFIX##_remove_nth(HT_Bag##TYPE_SUFFIX* bag, size_t n); \
+    HT_API HT_ErrorCode ht_bag##METHOD_SUFFIX##_add(HT_Bag##TYPE_SUFFIX* bag, TYPE data); \
+    HT_API void ht_bag##METHOD_SUFFIX##_clear(HT_Bag##TYPE_SUFFIX* bag);
 
-/**
- * Initializes HT_Bag structure.
- *
- * @param bag a pointer to the container.
- * @param min_capacity minimal capacity of the container.
- *
- * @returns #HT_ERR_OK, if initialization completed successfully; otherwise, appropriate error code.
- *
- * Initialization might fail, if the process can't allocate enough memory.
- * In that case, #HT_ERR_OUT_OF_MEMORY is returned.
- */
-HT_API HT_ErrorCode ht_bag_init(HT_Bag* bag, size_t min_capacity);
-
-/**
- * Uninitializes bag structure.
- *
- * This function releases all the memory allocated for
- * the bag and resets all the fields. After this function
- * call, @a bag should no longer be used (unless ht_bag_init()
- * is called on the same object).
- *
- * @param bag a pointer to the container.
- */
-HT_API void ht_bag_deinit(HT_Bag* bag);
-
-/**
- * Removes element from the container.
- *
- * The function removes all entries which equal
- * @a data value.
- *
- * @param bag a pointer to the container.
- * @param data an object to remove.
- */
-HT_API void ht_bag_remove(HT_Bag* bag, void* data);
-
-/**
- * Removes nth element from the container.
- *
- * @param bag a pointer to the container.
- * @param n an index of element to remove.
- */
-HT_API void ht_bag_remove_nth(HT_Bag* bag, size_t n);
-
-/**
- * Adds element to the end of the container.
- *
- * @param bag a pointer to the container.
- * @param data a value to be added to the container.
- *
- * @returns #HT_ERR_OK, if element was successfully added to the container; otherwise, appropriate error code.
- *
- * The function might fail, if the process can't allocate enough memory to extend internal data
- * buffer (#HT_ERR_OUT_OF_MEMORY). The function never fails, if ht_bag_init() doesn't fail
- * and number of elements in the bag is less than @a min_capacity.
- */
-HT_API HT_ErrorCode ht_bag_add(HT_Bag* bag, void* data);
-
-/**
- * Removes all elements from the container.
- *
- * @param bag a pointer to the container.
- */
-HT_API void ht_bag_clear(HT_Bag* bag);
-
-/**
- * Gets last element from the container.
- *
- * @param bag a pointer to the container.
- *
- * @warning If the @a bag is empty, behavior of this function
- * is undefined.
- */
 #define ht_bag_last(bag) bag.data[bag.size - 1]
+
+HT_DECLARE_BAG_TYPE(,, void*)
 
 HT_DECLS_END
 
