d74723d58c4f2aba41916cc63b74c12e89026753
Marcin Kolny <marcin.kolny@gmail.com>
2020-04-17 14:48:40 +0100

    [lib] build tcp listener conditionally

--- lib/include/hawktracer/listeners.h
+++ lib/include/hawktracer/listeners.h
@@ -1,7 +1,12 @@
 #ifndef HAWKTRACER_LISTENERS_H
 #define HAWKTRACER_LISTENERS_H
 
+#include <hawktracer/ht_config.h>
+
 #include <hawktracer/listeners/file_dump_listener.h>
+
+#ifdef HT_ENABLE_TCP_LISTENER
 #include <hawktracer/listeners/tcp_listener.h>
+#endif
 
 #endif /* HAWKTRACER_LISTENERS_H */
